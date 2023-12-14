71bffb9f095a1f0980250b252d2e9c3c6e399b96
FabbPP <140754765+FabbPP@users.noreply.github.com>
2023-12-14 09:46:33 -0500

    Update Juego.java
    size ventana

--- VideoJuego/paquete/Juego.java
+++ VideoJuego/paquete/Juego.java
@@ -5,7 +5,7 @@ public class Juego extends JFrame{
     public static void main(String[] args){
         Juego videoJuego = new Juego();
         videoJuego.setLocation(300, 200);
-        videoJuego.setSize(800, 800);
+        videoJuego.setSize(800, 700);
         videoJuego.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         PanelFondo tablero = new PanelFondo(800, 30);
         videoJuego.add(tablero);
