46fc5b272f0c0439aa1d59a361db7d38d458ee8a
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com.>
2023-12-13 22:22:01 -0500

    Crear clase piratas y arma

---
+++ VideoJuego/Piratas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package piratas;
+
+public class Piratas {
+
+    private int vida;
+    private String nombre; 
+    private int nivel; 
+    private double oro; 
+    private boolean estaVivo; 
+    private Arma arma; 
+    
+}
