becf5300bf444bd86c5bb43ee3c91b417388f018
DeniseHuacani <139657748+DeniseHuacani@users.noreply.github.com>
2023-12-27 19:09:26 -0500

    Update SegundaVentana.java

--- VideoJuego/SegundaVentana.java
+++ VideoJuego/SegundaVentana.java
@@ -5,10 +5,13 @@
 package trabajofinal;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.awt.event.*;
 
 public class SegundaVentana extends JFrame {
         
-    
+    private JButton btnEmpezar;
     
     public SegundaVentana() {
         setTitle("Información de Jugadores");
@@ -17,25 +20,49 @@ public class SegundaVentana extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
         
-        setLayout(new GridLayout(2, 2));
+        setLayout(new FlowLayout());
 
-        agregarJugador("Jugador 1", "img.jpg");
-        agregarJugador("Jugador 2", "imagen2.jpg");
-
+        agregarJugador("Jugador 1", "https://raw.githubusercontent.com/DeniseHuacani/TrabajoVideoJuego/main/imagenes/fondo4.png");
+        agregarJugador("Jugador 2", "https://raw.githubusercontent.com/DeniseHuacani/TrabajoVideoJuego/main/imagenes/fondo5.png");
+        btnEmpezar = new JButton("Empezar");
+        btnEmpezar.addActionListener(new Listener());
+        add(btnEmpezar);
         setLocationRelativeTo(null);
         setVisible(true);
     }
+    private class Listener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            if(e.getSource()==btnEmpezar){
+                dispose(); //para que se cierre la ventana
+                
+                Juego.main(new String[]{});
+            }
+            
+        }
+    }
 
-    private void agregarJugador(String nombre, String imagen) {
+    private void agregarJugador(String nombre, String urlImagen) {
         JPanel panelJugador = new JPanel();
-        panelJugador.setLayout(new BorderLayout());
+        panelJugador.setLayout(new GridLayout(0,1,10,45));
 
         JLabel labelNombre = new JLabel(nombre);
-        JLabel labelImagen = new JLabel(new ImageIcon(imagen));
+        JLabel labelImagen = new JLabel();
 
-        panelJugador.add(labelNombre, BorderLayout.NORTH);
-        panelJugador.add(labelImagen, BorderLayout.CENTER);
+        try {
+            ImageIcon imageIcon = new ImageIcon(new URL(urlImagen));
+            Image image = imageIcon.getImage();
+            Image nuevaImagen = image.getScaledInstance(100, 100, Image.SCALE_SMOOTH); // Ajusta el tamaño según sea necesario
+            labelImagen.setIcon(new ImageIcon(nuevaImagen));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        panelJugador.add(labelNombre);
+        panelJugador.add(labelImagen);
 
         add(panelJugador);
+    }
+    public static void main (String[] args){
+            new SegundaVentana();
     }
 }
